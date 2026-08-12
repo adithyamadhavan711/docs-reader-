1f466d61ac321936f3ff587a492e8287294847ef
Adithya <adithyamadhavan11@gmail.com>
2026-08-12 17:47:58 +0530

    changes to main.py

--- docs.py
+++ docs.py
@@ -6,49 +6,63 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = [
     "https://www.googleapis.com/auth/drive.readonly",
-    "https://www.googleapis.com/auth/documents.readonly"
+    "https://www.googleapis.com/auth/documents.readonly",
 ]
 
-# The ID of a sample document.
-DOCUMENT_ID = "1SpLDRO9oDo17ufaLr9f-WUpSEUq5K38ajEsESAV_zGM"
+
+def documentcontent(document_id):
+
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+
+    service = build("docs", "v1", credentials=creds)
+
+    document = service.documents().get(documentId=document_id).execute()
+
+    title = document["title"]
+
+    text = ""
+
+    for item in document["body"]["content"]:
+
+        if "paragraph" in item:
+
+            for element in item["paragraph"]["elements"]:
+
+                if "textRun" in element:
+                    text += element["textRun"]["content"]
+
+    return title, text
 
 
 def main():
-  """Shows basic usage of the Docs API.
-  Prints the title of a sample document.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
 
-  try:
-    service = build("docs", "v1", credentials=creds)
+    creds = None
 
-    # Retrieve the documents contents from the Docs service.
-    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
 
-    print(f"The title of the document is: {document.get('title')}")
-  except HttpError as err:
-    print(err)
+    if not creds or not creds.valid:
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+
+            creds = flow.run_local_server(port=0)
+
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+
+        service = build("docs", "v1", credentials=creds)
+
+    except HttpError as err:
+        print(err)
 
 
 if __name__ == "__main__":
-  main()
+    main()
--- main.py
+++ main.py
@@ -1,28 +1,74 @@
+from drive import get_documents_drive
+from docs import documentcontent
+from sqlite import get_doc, insert, update_doc, delete_doc, get_all_ids, search_doc
+import argparse
 
-from google.oauth2.credentials import Credentials    
-    
-from googleapiclient.discovery import build
+parser = argparse.ArgumentParser()
 
-SCOPES = [
-    "https://www.googleapis.com/auth/documents.readonly"
-]
+parser.add_argument("--command", choices=["load", "search"], required=True)
 
-creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+parser.add_argument("--word")
 
-docs = build("docs", "v1", credentials=creds)
+args = parser.parse_args()
 
-DOCUMENT_ID = "1SpLDRO9oDo17ufaLr9f-WUpSEUq5K38ajEsESAV_zGM"
+if args.command == "load":
 
-document = docs.documents().get(documentId=DOCUMENT_ID).execute()
+    documents = get_documents_drive()
 
-print("Title:", document["title"])
+    google_drive_id = []
 
-text = ""
+    # goes thru every single doc and gets id and modified time and saves
 
-for item in document["body"]["content"]:
-    if "paragraph" in item:
-        for element in item["paragraph"]["elements"]:
-            if "textRun" in element:
-                text += element["textRun"]["content"]
+    for document in documents:
 
-print(text)
+        doc_id = document["id"]
+        modified_time = document["modifiedTime"]
+        google_drive_id.append(doc_id)
+
+        # gets the id and if it returns none (new doc)
+        old_id = get_doc(doc_id)
+
+        if old_id is None:
+
+            # gets the title and content
+            title, content = documentcontent(doc_id)
+
+            insert(doc_id, title, content, modified_time)
+
+            print("Added:", title)
+
+        # the document exists in the sqlite db but is changed (compare modified time)
+        elif old_id[3] != modified_time:
+
+            title, content = documentcontent(doc_id)
+
+            update_doc(doc_id, title, content, modified_time)
+
+            print("Updated:", title)
+
+        # nothing was changed
+        else:
+
+            print("No change:", old_id[1])
+
+    for document in get_all_ids():
+
+        # storin all db ids
+        db_id = document[0]
+
+        # if the id is not in google_Drive_ID then delete that doc
+        if db_id not in google_drive_id:
+
+            delete_doc(db_id)
+
+            print("Deleted:", db_id)
+
+
+elif args.command == "search":
+
+    results = search_doc(args.word)
+
+    for title, content in results:
+
+        print("Title:", title)
+        print(content)
